fd289d1793a4126086adaa680c5efa32f1e20e6d
CarHSMig <151793880+CarHSMig@users.noreply.github.com>
2024-04-14 16:17:38 -0300

    Update TesteCarro.java

--- AtividadeRevisao1Carro/src/TesteCarro.java
+++ AtividadeRevisao1Carro/src/TesteCarro.java
@@ -7,14 +7,11 @@ public class TesteCarro {
         novoCarro.setModelo("Palio");
         novoCarro.setCor("Prata");
 
-        // Exibindo as informações do carro novamente
         System.out.println("Após modificações:");
         novoCarro.exibirInformacoes();
 
-        // Criando outro carro
         Carro carroDois = new Carro("Volkswagen", "Amarok", 2020, "Azul");
 
-        // Exibindo as informações do carro do amigo
         System.out.println("Carro do amigo:");
         carroDois.exibirInformacoes();
     }
